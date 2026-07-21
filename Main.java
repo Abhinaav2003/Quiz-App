5d9d76aa0980eb556e0482e3f45345f8497709e2
Abhinaav2003 <144618349+Abhinaav2003@users.noreply.github.com>
2026-07-21 21:35:08 +0530

    a

--- Main.java
+++ Main.java
@@ -4,39 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // Add some default questions
-        QuestionStore.questions.add(new Question(
-                "What is Java?",
-                "Programming Language",
-                "Operating System",
-                "Database",
-                "Browser",
-                "Programming Language"));
-
-        QuestionStore.questions.add(new Question(
-                "Which company developed Java?",
-                "Microsoft",
-                "Oracle",
-                "Google",
-                "Apple",
-                "Oracle"));
-
-        QuestionStore.questions.add(new Question(
-                "Which keyword is used to inherit a class?",
-                "implements",
-                "extends",
-                "import",
-                "package",
-                "extends"));
-
-        QuestionStore.questions.add(new Question(
-                "Which package is used for Swing?",
-                "java.sql",
-                "java.io",
-                "javax.swing",
-                "java.net",
-                "javax.swing"));
-
         // Launch Login Window
         SwingUtilities.invokeLater(() -> {
             new LoginFrame();
